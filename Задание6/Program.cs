ad88381d95ed118627abf73d640cd0cf9f3c20bb
Aleksey <leshkaalpatov@mail.ru>
2022-02-04 16:01:20 +0300

    Homework6

---
+++ Задание6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число четным.
+
+int a = new Random().Next(1, 50);
+
+if (a % 2 == 0)
+{
+    Console.Write(a);
+    Console.Write(" - четное");
+}
+else
+{
+    Console.Write(a);
+    Console.Write(" - нечетное");
+}
