19a1acfd14b0e2f28b147dcf55ca9b11798c57d1
Aleksey <leshkaalpatov@mail.ru>
2022-02-04 16:02:14 +0300

    Homework8

---
+++ Задание8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N.
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+int n = array.Length;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] % 2 == 0)
+    {
+        Console.WriteLine(array[index]);
+    }    
+    index++;
+}
