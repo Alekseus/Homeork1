2eb6e900843096dd7f17b74929e7b7afa0bf43cc
Aleksey <leshkaalpatov@mail.ru>
2022-02-04 15:59:24 +0300

    Homeork2

---
+++ Задание2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+Console.WriteLine(a);
+Console.WriteLine(b);
+if (a > b)
+{
+    Console.Write("Это число больше: ");
+    Console.Write(a);
+    Console.WriteLine();
+    Console.Write("Это число меньше: ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write("Это число больше: ");
+    Console.Write(b);
+    Console.WriteLine();
+    Console.Write("Это число меньше: ");
+    Console.Write(a);
+}
+
+// Найти максимальное из трех чисел.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11, 211, 31};
+
+int result = Max(array[0], array[1], array[2]);
+
+
+Console.WriteLine(result);
